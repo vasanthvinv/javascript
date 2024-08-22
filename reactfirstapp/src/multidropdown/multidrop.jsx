df5c9e2098e87a7c6f5201c43b43b9048a3c26bd
root <root@vasanth.>
2024-08-22 07:30:26 +0530

    multidropdown updated

---
+++ reactfirstapp/src/multidropdown/multidrop.jsx
@@ -0,0 +1,74 @@
+// import React, { useState } from 'react';
+
+// const data = [
+//     {
+//       fruit: [
+//         { apple: ['greenApple', 'redApple', 'whiteApple'] },
+//         { orange: ['kamala', 'bloodOrange', 'jaffaOrange'] },
+//         { mango: ['alphonso', 'irwin', 'keitt'] },
+//       ],
+//     },
+//     {
+//       animals: [
+//         { lion: ['maleLion', 'femaleLion', 'cub'] },
+//         { tiger: ['bengalTiger', 'siberianTiger', 'sumatranTiger'] },
+//         { elephant: ['africanElephant', 'asianElephant'] },
+//       ],
+//     },
+//     {
+//       plants: [
+//         { rose: ['redRose', 'whiteRose', 'yellowRose'] },
+//         { lily: ['whiteLily', 'pinkLily', 'orangeLily'] },
+//         { sunflower: ['giantSunflower', 'commonSunflower', 'dwarfSunflower'] },
+//       ],
+//     },
+//   ];
+
+// function Multidrop() {
+    
+//   const [isOpen, setIsOpen] = useState(null);
+//   const [selected, setSelected] = useState(null);
+
+//   const handleDrop = (i) => {
+//     setIsOpen(isOpen === i ? null : i);
+//   };
+
+//   const handleClick = (i) => {
+//     setSelected(selected === i ? null : i);  };
+
+//   return (
+//     <div>
+//       {data.map((item, i) => (
+//         <div key={i}>
+//           <ul onClick={() => handleDrop(i)}>
+//             {Object.keys(item)}
+//           </ul>
+//           {isOpen === i && (
+//             <ul>
+//               {Object.values(item)[0].map((object, index) => (
+//                 <div key={index}>
+//                   {Object.keys(object).map((key,i) => (
+//                     <div key={i}>
+//                       <li onClick={() => handleClick(key)}>
+//                         {key}
+//                       </li>
+//                       { selected === key && (
+//                         <ul>
+//                           {object[key].map((item, index) => (
+//                             <li key={index}>{item}</li>
+//                           ))}
+//                         </ul>
+//                       )}
+//                     </div>
+//                   ))}
+//                 </div>
+//               ))}
+//             </ul>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Multidrop;
